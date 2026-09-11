e51233b34748a2bb67cf640845091a76b4ad923a
Bezrukix <didriknikita@gmail.com>
2026-09-11 10:24:42 +0300

    Поменял название приветсявия с пользователем

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,11 +8,10 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-
     Library library;
 
     cout << "\n===========================================" << endl;
-    cout << "   Добро пожаловать в Библиотеку!" << endl;
+    cout << "   Welcome to the Library!" << endl;
     cout << "===========================================" << endl;
 
     run_menu(library);
